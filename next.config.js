017f3d667cdaf518a14ca9d4d0c9cfc1702fc441
zerox-toml <passionkaizen96111@gmail.com>
2025-08-30 04:46:29 -0500

    feat:update netlify.toml

--- next.config.js
+++ next.config.js
@@ -2,8 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  // Add these configurations for Netlify deployment
+  output: 'export', // Enable static export
+  trailingSlash: true,
   images: {
     domains: ["media.graphassets.com", "res.cloudinary.com", "media.dev.to"],
+    unoptimized: true, // This helps with Netlify deployment
   },
 }
 
