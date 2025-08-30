95192e5560a3e0796511b9697705718a89bafb6e
zerox-toml <passionkaizen96111@gmail.com>
2025-08-30 04:50:30 -0500

    fix:path err

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  // Add these configurations for Netlify deployment
-  output: 'export', // Enable static export
-  trailingSlash: true,
   images: {
     domains: ["media.graphassets.com", "res.cloudinary.com", "media.dev.to"],
     unoptimized: true, // This helps with Netlify deployment
